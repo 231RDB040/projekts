c61448dbd09d9450c147cfa4625a3cffd54eba10
231RDB040 <144886660+231RDB040@users.noreply.github.com>
2024-01-19 00:25:14 +0200

    Add files via upload

--- source.py
+++ source.py
@@ -1,1 +1,28 @@
+import selenium
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 
+import time
+
+service = Service()
+option = webdriver.ChromeOptions()
+driver = webdriver.Chrome(service=service, options=option)
+
+url = "https://www.jauns.lv"
+driver.get(url)
+time.sleep(2)
+
+find=driver.find_element(By.CLASS_NAME, "css-47sehv")
+find.click()
+print()
+print("·····················································································································")
+a = driver.find_elements(By.CLASS_NAME, "streamer")
+
+for elements in a:
+    nosaukums = elements.find_element(By.CLASS_NAME, "article-simple__title").text
+    links = elements.find_element(By.CLASS_NAME, "article-simple__link").get_attribute("href")
+    print(nosaukums)
+    print()
+    print(links)
+    print("·····················································································································")
